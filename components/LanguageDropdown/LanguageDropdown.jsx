029fec6e7f283fc26415a5ac831b41d853b67717
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-11-22 18:21:53 +0100

    try langs select

--- components/LanguageDropdown/LanguageDropdown.jsx
+++ components/LanguageDropdown/LanguageDropdown.jsx
@@ -4,7 +4,7 @@ import styles from './LanguageDropdown.module.scss';
 import Link from 'next/link';
 
 const LanguageDropdown = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState('EN'); // Set "EN" as the default language
+  const [selectedLanguage, setSelectedLanguage] = useState('EN');
 
   const toggleDropdown = () => {
     // Add code to toggle the dropdown if needed
@@ -12,6 +12,16 @@ const LanguageDropdown = () => {
 
   const selectLanguage = (language) => {
     setSelectedLanguage(language);
+
+    // Get the current URL
+    const currentUrl = window.location.href;
+
+    // Replace "ru." with an empty string to remove it from the URL
+    const newUrl = currentUrl.replace('ru.', '');
+
+    // Change the URL without reloading the page
+    window.history.replaceState({}, document.title, newUrl);
+
     // You can also add logic to change the language in your app here
   };
 
@@ -21,8 +31,10 @@ const LanguageDropdown = () => {
         {selectedLanguage}
       </div>
       <div className={styles.dropdownMenu}>
-        {/* <Link href="#" className={styles.dropdownItem} onClick={() => selectLanguage('EN')}>EN</Link> */}
-        <Link href="#" className={styles.dropdownItem} onClick={() => selectLanguage('RU')}>RU</Link>
+        <Link href="#" className={styles.dropdownItem} onClick={() => selectLanguage('RU')}>
+          RU
+        </Link>
+        {/* Add other language links as needed */}
       </div>
     </div>
   );
