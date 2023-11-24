404c71d87721e83082daecf92f9bff030a7a3178
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-11-24 14:53:13 +0100

    mod lang

--- components/LanguageDropdown/LanguageDropdown.jsx
+++ components/LanguageDropdown/LanguageDropdown.jsx
@@ -1,10 +1,14 @@
-'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './LanguageDropdown.module.scss';
 import Link from 'next/link';
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const LanguageDropdown = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState('RU'); // Set "EN" as the default language
+  const [selectedLanguage, setSelectedLanguage] = useState('РУС');
+  const [hasScrolled, setHasScrolled] = useState(false);
 
   const toggleDropdown = () => {
     // Add code to toggle the dropdown if needed
@@ -15,14 +19,42 @@ const LanguageDropdown = () => {
     // You can also add logic to change the language in your app here
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setHasScrolled(true);
+      } else {
+        setHasScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    ScrollTrigger.create({
+      start: 'top -70',
+      end: 99999,
+      toggleClass: {
+        className: styles.dropdownScrolled,
+        targets: `.${styles.dropdownItem}`
+      }
+    });
+  }, []);
+
   return (
     <div className={styles.dropdown}>
       <div className={styles.dropdownToggle} onClick={toggleDropdown}>
         {selectedLanguage}
       </div>
       <div className={styles.dropdownMenu}>
-        {/* <Link href="#" className={styles.dropdownItem} onClick={() => selectLanguage('EN')}>EN</Link> */}
-        <Link href="https://academgo.com" className={styles.dropdownItem} onClick={() => selectLanguage('EN')}>EN</Link>
+        <Link href="https://academgo.com" className={`${styles.dropdownItem} ${hasScrolled ? styles.dropdownScrolled : ''}`} onClick={() => selectLanguage('АНГ')}>
+          АНГ
+        </Link>
       </div>
     </div>
   );
